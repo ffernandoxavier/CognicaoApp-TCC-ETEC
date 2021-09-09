f666016743b87966aa25af5ee15a989aa35b784e
Fernando Xavier <ffernandoxavier@outlook.com>
2021-09-09 12:02:52 -0300

    Iniciando layout

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -17,7 +17,38 @@
         <script src="/js/script.js"></script>
         
     </head>
-    <body>
-    
+    <body data-spy="scroll" data-target=".fixed-top">
+        <nav class="navbar navbar-expand-lg fixed-top trans-navigation">
+        <div class="container">
+        <a class="navbar-brand" href="welcome">
+            <img src="img/logo002.png" alt="" class="img-fluid b-logo">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNav" aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon">
+                <i class="fa fa-bars"></i>
+            </span>
+          </button>
+
+        <div class="collapse navbar-collapse justify-content-end" id="mainNav">
+            <ul class="navbar-nav ">
+                <li class="nav-item">
+                    <a class="nav-link active smoth-scroll" href=".banner-area">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link smoth-scroll" href="#intro">Benefits</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link smoth-scroll" href="#about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link smoth-scroll" href="#service">Service</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link smoth-scroll" href="#pricing">Pricing</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
     </body>
 </html>
