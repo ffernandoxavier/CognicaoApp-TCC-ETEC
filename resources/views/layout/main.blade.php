629d76e44439bcb9972a40d7bd4d1c6c1e9ce307
Fernando Xavier <ffernandoxavier@outlook.com>
2021-09-09 11:05:10 -0300

    Testes01

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -6,22 +6,18 @@
 
         <title>@yield('title')</title>
 
-        <!--FONTES DA APLICAÇÃO -->
+        <!--FONTES DA APLICAÇÃO-->
         <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
-
-        <!--CSS BOOTSTRAP -->
+        
+        <!--CSS BOOTSTRAP--> 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
         
-        <!-- CSS DA APLICAÇÃO -->
+        <!--CSS DA APLICAÇÃO-->
         <link rel="stylesheet" href="/css/style.css">
         <script src="/js/script.js"></script>
-
+        
     </head>
     <body>
-    @yield('content')
-    <footer>
-        <p>MAPEIAMENTE &copy; 2021</P> 
-    </footer>
-
+    
     </body>
 </html>
