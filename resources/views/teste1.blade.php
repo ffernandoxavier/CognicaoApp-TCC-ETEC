de33ed7b0abcb6df56ed995e5946234025e3e880
Fernando Xavier <ffernandoxavier@outlook.com>
2021-10-27 17:20:42 -0300

    Atualizações dos testes

--- resources/views/teste1.blade.php
+++ resources/views/teste1.blade.php
@@ -12,83 +12,83 @@
   <p id="teste1cx">Para começar selecione o DIA, MÊS e ANO,</p>
   <p id="teste2cx"> que você está realizando o teste.</p>
   </main>
-  <main id="flex-centralizado">
-  <div class="half-box">
-  <label for ="DIA"> DIA </label><br>
-     <select name="format" id="format" style="width:100px; color: black; text-align:center;">
-      <option value="0"></option>
-      <option value="1">1</option>
-      <option value="2">2</option>
-      <option value="3">3</option>
-      <option value="4">4</option>
-      <option value="5">5</option>
-      <option value="6">6</option>
-      <option value="7">7</option>
-      <option value="8">8</option>
-      <option value="9">9</option>
-      <option value="10">10</option>
-      <option value="11">11</option>
-      <option value="12">12</option>
-      <option value="13">13</option>
-      <option value="14">14</option>
-      <option value="15">15</option>
-      <option value="16">16</option>
-      <option value="17">17</option>
-      <option value="18">18</option>
-      <option value="19">19</option>
-      <option value="20">20</option>
-      <option value="21">21</option>
-      <option value="22">22</option>
-      <option value="23">23</option>
-      <option value="24">24</option>
-      <option value="25">25</option>
-      <option value="26">26</option>
-      <option value="27">27</option>
-      <option value="28">28</option>
-      <option value="29">29</option>
-      <option value="30">30</option>
-      <option value="31">31</option>
-     </select>
-  </div>
-  <div class="half-box">
-  <label for ="MÊS"> MÊS </label><br>
-     <select style="width:100px; text-align:center;">
-      <option value="0"></option>
-      <option value="1">1</option>
-      <option value="2">2</option>
-      <option value="3">3</option>
-      <option value="4">4</option>
-      <option value="5">5</option>
-      <option value="6">6</option>
-      <option value="7">7</option>
-      <option value="8">8</option>
-      <option value="9">9</option>
-      <option value="10">10</option>
-      <option value="11">11</option>
-      <option value="12">12</option>
-    </select>
-  </div>
-  <div class="half-box">
-  <label for ="ANO"> ANO </label><br>
-     <select style="width:100px text-align:center;">
-      <option value="0"></option>
-      <option value="2010">2010</option>
-      <option value="2011">2011</option>
-      <option value="2012">2012</option>
-      <option value="2013">2013</option>
-      <option value="2014">2014</option>
-      <option value="2015">2015</option>
-      <option value="2016">2016</option>
-      <option value="2017">2017</option>
-      <option value="2018">2018</option>
-      <option value="2019">2019</option>
-      <option value="2020">2020</option>
-      <option value="2021">2021</option>
-    </select>
-  </div>
+  <main>
+    <form name="teste11" action='#' method='get'>
+      <div class="half-box">
+        <label for ="DIA" > DIA </label>
+          <select name="f_dia" required id="format" style="width:100px; color: black; text-align:center;">
+            <option value=""></option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+            <option value="13">13</option>
+            <option value="14">14</option>
+            <option value="15">15</option>
+            <option value="16">16</option>
+            <option value="17">17</option>
+            <option value="18">18</option>
+            <option value="19">19</option>
+            <option value="20">20</option>
+            <option value="21">21</option>
+            <option value="22">22</option>
+            <option value="23">23</option>
+            <option value="24">24</option>
+            <option value="25">25</option>
+            <option value="26">26</option>
+            <option value="27">27</option>
+            <option value="28">28</option>
+            <option value="29">29</option>
+            <option value="30">30</option>
+            <option value="31">31</option>
+          </select>
+      </div>
+      <div class="half-box">
+        <label for ="MÊS" > MÊS </label>
+          <select name="f_mes" required style="width:100px; text-align:center;">
+            <option value=""></option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+          </select>
+      </div>
+      <div class="half-box">
+       <label for ="ANO"> ANO </label>
+        <select name="f_ano" required style="width:100px text-align:center;">
+          <option value=""></option>
+          <option value="2010">2010</option>
+          <option value="2011">2011</option>
+          <option value="2012">2012</option>
+          <option value="2013">2013</option>
+          <option value="2014">2014</option>
+          <option value="2015">2015</option>
+          <option value="2016">2016</option>
+          <option value="2017">2017</option>
+          <option value="2018">2018</option>
+          <option value="2019">2019</option>
+          <option value="2020">2020</option>
+          <option value="2021">2021</option>
+        </select>
+      </div>
+      <input class="form2" type="submit" name="form2enviar" value="Enviar">
+    </form>
   </main>
-  <div id="imgseta1">
-      <a href="teste2" ><img src="img/seta.png"></a>
-  </div>
 </body>
 </html>
--- resources/views/teste2.blade.php
+++ resources/views/teste2.blade.php
@@ -9,38 +9,41 @@
 </head>
 <body>
     <main class="main-main">
+    <form name="teste11" action='#' method='get'>
     <p id="textt2">Qual o dia da semana que estamos?</p>
+    <br>
         <div class="custom-radio">
-            <input id="radio-1" name="semana" value="1" type="radio"/> 
+            <input id="radio-1" name="semana" value="1" type="radio" /> 
             <label id="txtrb" for="radio-1"> Domingo </label>
         </div>
         <div class="custom-radio">
-            <input id="radio-2" name="semana" value="2" type="radio"/> 
+            <input id="radio-2" name="semana" value="2" type="radio" /> 
             <label id="txtrb" for="radio-2"> Segunda - Feira </label>
         </div>
         <div class="custom-radio">
-            <input id="radio-3" name="semana" value="3" type="radio"/> 
+            <input id="radio-3" name="semana" value="3" type="radio" /> 
             <label id="txtrb" for="radio-3"> Terça - Feira </label>
         </div>
         <div class="custom-radio">
-            <input id="radio-4" name="semana" value="4" type="radio"/> 
+            <input id="radio-4" name="semana" value="4" type="radio" /> 
             <label id="txtrb" for="radio-4"> Quarta - Feira </label>
         </div>
         <div class="custom-radio">
-            <input id="radio-5" name="semana" value="5" type="radio"/> 
+            <input id="radio-5" name="semana" value="5" type="radio" /> 
             <label id="txtrb" for="radio-5"> Quinta - Feira </label>
         </div>
         <div class="custom-radio">
-            <input id="radio-6" name="semana" value="6" type="radio"/> 
+            <input id="radio-6" name="semana" value="6" type="radio" /> 
             <label id="txtrb" for="radio-6"> Sexta- Feira </label>
         </div>
         <div class="custom-radio">
             <input id="radio-7" name="semana" value="7" type="radio"/> 
             <label id="txtrb" for="radio-7"> Sábado </label>
         </div>
+        <br>
+        <br>
+        <input class="form2" type="submit" name="form2enviar" value="Enviar">
+</form>
     </main>
-    <div id="imgseta1tt">
-      <a href="teste3" ><img src="img/seta.png"></a>
-    </div>
 </body>
 </html>
