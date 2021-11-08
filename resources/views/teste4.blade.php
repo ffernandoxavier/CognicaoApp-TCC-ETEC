6c6f2dc784b4e04a7b116230adecac6974668a8f
Fernando Xavier <sfexavier@gmail.com>
2021-11-08 19:02:13 -0300

    org

--- resources/views/teste4.blade.php
+++ resources/views/teste4.blade.php
@@ -3,15 +3,14 @@
     <meta charset="UTF-8">
     <meta name=viewport content="width=\, initial-scale=1.0">
     <title>Mapeiamente</title>
-    <meta http-equiv="refresh" content="5; URL='teste5'"/>
+    <meta http-equiv="refresh" content="7; URL='teste5'"/>
 
     <!-- LINK CSS --> 
     <link rel="stylesheet" href="/css/estilo.css"> 
 </head>
 <body>
     <main>
-    <p id="textt4">LEIA A FRASE ABAIXO: </p>
-    <br>
+    <p id="textt4">Você terá 5 segundos para ler a frase abaixo!</p>
     <br>
     <br>
     <p id="textt4">Ex:  "HOJE EU ACORDEI SORRINDO!"</p>
